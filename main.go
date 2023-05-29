4c47232773e0f96d1c05032660363fd8ebf553d9
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-05-29 02:32:14 +0200

    Add per-domain net allowlist.

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -29,6 +31,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/google/go-github/v52/github"
+	"github.com/miekg/dns"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/sqlbunny/errors"
@@ -36,11 +39,12 @@ import (
 )
 
 type Config struct {
-	DataDir     string       `yaml:"data_dir"`
-	ExternalURL string       `yaml:"external_url"`
-	ListenPort  int          `yaml:"listen_port"`
-	Image       string       `yaml:"image"`
-	Github      GithubConfig `yaml:"github"`
+	DataDir        string       `yaml:"data_dir"`
+	ExternalURL    string       `yaml:"external_url"`
+	ListenPort     int          `yaml:"listen_port"`
+	AllowedDomains []string     `yaml:"allowed_domains"`
+	Image          string       `yaml:"image"`
+	Github         GithubConfig `yaml:"github"`
 }
 
 type GithubConfig struct {
@@ -525,6 +529,7 @@ func (s *Service) runJobInner(ctx context.Context, job *Job, gh *github.Client, 
 			oci.WithEnv([]string{
 				"HOME=/ci",
 			}),
+			oci.WithNamespacedCgroup(),
 			oci.WithHostNamespace(specs.NetworkNamespace), // TODO network sandboxing
 			oci.WithMounts([]specs.Mount{
 				{
@@ -541,7 +546,7 @@ func (s *Service) runJobInner(ctx context.Context, job *Job, gh *github.Client, 
 				},
 				{
 					Type:        "none",
-					Source:      "/etc/resolv.conf",
+					Source:      filepath.Join(s.config.DataDir, "resolv.conf"),
 					Destination: "/etc/resolv.conf",
 					Options:     []string{"rbind", "ro"},
 				},
@@ -650,7 +655,148 @@ func (s *Service) HandleJobLogs(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// example.com matches example.com but not foo.example.com
+// *.example.com matches foo.example.com and example.com
+// take care about dots.
+func domainMatches(domain string, pattern string) bool {
+	// Add trailing dot to both domain and pattern to make matching easier
+	if !strings.HasSuffix(pattern, ".") {
+		pattern += "."
+	}
+	if !strings.HasSuffix(domain, ".") {
+		domain += "."
+	}
+
+	if domain == pattern {
+		return true
+	}
+	if "*."+domain == pattern {
+		return true
+	}
+	if strings.HasPrefix(pattern, "*.") && strings.HasSuffix(domain, pattern[1:]) {
+		return true
+	}
+	return false
+}
+
+func (s *Service) domainAllowed(domain string) bool {
+	for _, d := range s.config.AllowedDomains {
+		if domainMatches(domain, d) {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *Service) handleDNSQuery(m *dns.Msg) {
+	for _, q := range m.Question {
+		switch q.Qtype {
+		case dns.TypeA:
+			log.Printf("Query for %s\n", q.Name)
+			if !s.domainAllowed(q.Name) {
+				log.Printf("Domain %s is not allowed\n", q.Name)
+				m.Rcode = dns.RcodeNameError
+				return
+			}
+
+			ips, err := net.LookupHost(q.Name)
+			if err != nil {
+				log.Printf("Failed to lookup host: %v\n", err)
+				m.Rcode = dns.RcodeServerFailure
+				return
+			}
+
+			for _, ip := range ips {
+				tryExec("nft", "add", "element", "inet", "bender", "allow", "{", ip, "}")
+
+				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+				if err != nil {
+					log.Printf("Failed to create RR: %v\n", err)
+					// ignore
+				} else {
+					m.Answer = append(m.Answer, rr)
+				}
+			}
+		}
+	}
+}
+
+func (s *Service) handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Compress = false
+
+	switch r.Opcode {
+	case dns.OpcodeQuery:
+		s.handleDNSQuery(m)
+	}
+
+	w.WriteMsg(m)
+}
+
+func (s *Service) runDNS() {
+	// attach request handler func
+	dns.HandleFunc(".", s.handleDNSRequest)
+
+	// start server
+	server := &dns.Server{Addr: "127.0.0.93:53", Net: "udp"}
+	log.Printf("Starting DNS server at %s", server.Addr)
+	err := server.ListenAndServe()
+	defer server.Shutdown()
+	if err != nil {
+		log.Fatalf("Failed to start DNS server: %s\n ", err.Error())
+	}
+}
+
+func tryExec(cmd string, args ...string) {
+	log.Printf("Executing command: %s %s", cmd, strings.Join(args, " "))
+	c := exec.Command(cmd, args...)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	err := c.Run()
+	if err != nil {
+		log.Printf("Failed to execute command: %v", err)
+	}
+}
+
+func (s *Service) setupNftables() {
+	c := exec.Command("nft", "-f", "-")
+	c.Stdin = strings.NewReader(`
+		table inet bender 
+		delete table inet bender
+		
+		table inet bender {
+			set allow {
+				type ipv4_addr
+				elements = { 127.0.0.93 }
+			}
+		
+			chain output {
+				type filter hook output priority 0; policy accept;
+				socket cgroupv2 level 1 "bender" goto bender-output
+			}
+		
+			chain bender-output {
+				ip daddr @allow accept
+				ip protocol tcp reject with tcp reset
+				reject with icmp type host-prohibited
+			}
+		}
+	`)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	err := c.Run()
+	if err != nil {
+		log.Fatalf("Failed to setup nftables: %v", err)
+	}
+
+}
+
 func (s *Service) Run() {
+	s.setupNftables()
+
+	go s.runDNS()
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/jobs/{jobID}", s.HandleJobLogs)
@@ -693,6 +839,8 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+
+	os.WriteFile(filepath.Join(config.DataDir, "resolv.conf"), []byte("nameserver 127.0.0.93"), 0644)
 
 	cntd, err := containerd.New("/run/containerd/containerd.sock")
 	if err != nil {
